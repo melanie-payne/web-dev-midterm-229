d6a111c24411aea7c58fe8f830c71626d5a8fbb6
krunalsukhwani <krunal.sukhwani@gmail.com>
2023-06-07 20:13:42 -0400

    Retrieve Documents in Console

---
+++ routes/product.js
@@ -0,0 +1,19 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+//connect to model
+let Product = require('../models/product');
+
+//Manage routes
+router.get('/', (req, res, next) => {
+    Product.find((err, productList) => {
+        if(err){
+            return console.error(err);
+        }else{
+            console.log(productList);
+        }
+    });
+});
+
+module.exports = router;
