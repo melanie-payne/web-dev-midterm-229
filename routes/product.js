b9dd4e1bdda8023e791f38fe3bbff6a86fea37b9
krunalsukhwani <68172724+krunalsukhwani@users.noreply.github.com>
2023-06-14 20:04:30 -0400

    Update Data

--- routes/product.js
+++ routes/product.js
@@ -42,4 +42,40 @@ router.post('/add', (req, res, next) => {
     });
 });
 
+//Retrieve data from MongoDB and Open it in view (FORM)
+router.get('/edit/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    Product.findById(id, (err, productToEdit) => {
+        if(err){
+            console.log(err);
+            res.end(err);
+        }else{
+            //write code to display data in view
+            res.render('product/edit', { title : 'Edit Product', product: productToEdit})
+        }
+    });
+});
+
+//write code to store updated data into MongoDB
+router.post('/edit/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    let updatedProduct = Product({
+        "_id": id,
+        "name": req.body.pname,
+        "company": req.body.pcompany,
+        "price": req.body.price
+    });
+
+    Product.updateOne({_id: id}, updatedProduct, (err) => {
+        if(err){
+            console.log(err);
+            res.end(err);
+        }else{
+            res.redirect('/products');
+        }
+    });
+});
+
 module.exports = router;
